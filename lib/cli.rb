e89714e7c28d899e038a18d8cc7c46d17b0343c1
Sebastián Salata R-T <SA.SalataRT@GMail.com>
2016-10-25 21:28:17 -0300

    move static methods from 'Todos' to 'Todo' class

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,5 @@
 require 'thor'
 require_relative 'todo'
-require_relative 'todos'
 
 class TodoCLI < Thor
   desc 'add', 'adds a TODO to the list according to the --title, --deadline and --body (optional) options'
@@ -9,14 +8,14 @@ class TodoCLI < Thor
   option :body
   def add
     todo = Todo.new(options[:title], options[:body], options[:deadline])
-    Todos.add(todo)
+    todo.save
   end
 
   desc 'ls', 'lists TODOS according to the --sort option, and filters overdue todos if the --overdue option is specified'
   option :sort
   option :overdue, type: :boolean
   def ls
-    rows = Todos.list(options[:sort], options[:overdue])
+    rows = Todo.list(options[:sort], options[:overdue])
     rows.each { |row| puts row.to_yaml }
   end
 end
